89762b4f35bbbaa3bf9a08d35a935c3b92eeb240
Devarsh Dubey <devarsh.d10@gmail.com>
2025-01-23 00:02:22 +0530

    Initial commit

---
+++ vdo1/new.js
@@ -0,0 +1,6 @@
+console.log("HElloe world")
+console.log("running node here")
+var a=45;
+var b=85;
+var c=a+b;
+console.log("Addition of a + b is " + c)
---
+++ vdo1/script.js
@@ -0,0 +1,14 @@
+alert("Welcome to the world");
+console.log("COde is running");
+console.log("Code is bhaging")
+console.log("Code is looking like a vow!")
+var a=prompt("Enter the number as you want")
+console.log("The number is "+ a)
+var istrue=confirm("Are YOu sure you want to blast your computer?")
+if(istrue){
+    console.log("Your computer is blasting")
+}
+else{
+    console.log("chal hatt");
+}
+document.body.style.backgroundColor="red"
