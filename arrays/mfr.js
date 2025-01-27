947b7de17fd0faf3dc3b3b6f3b4149bc23ff29d6
Devarsh Dubey <devarsh.d10@gmail.com>
2025-01-27 22:50:12 +0530

    .

--- arrays/mfr.js
+++ arrays/mfr.js
@@ -1,4 +1,4 @@
-let arr=[1,2,3,4,5,6,88,66,85];
+let arr=[1,2,3,4,5,];
 // let newarr=[]
 // for (let index = 0; index < arr.length; index++) {
 //     const element=arr[index];
@@ -7,15 +7,32 @@ let arr=[1,2,3,4,5,6,88,66,85];
 // }
 //console.log(newarr); return the reverse array
 
-let newarr=arr.map(  (value)=>{
-    return value**2;
-})
-console.log(newarr)
+// let newarr=arr.map(  (value)=>{
+//     return value**2;
+// })
+// console.log(newarr)
 
-const greaterthanseven = (e)=>{
-    if(e>7){
+// const greaterthanseven = (e)=>{
+//     if(e>7){
+//         return true;
+//     }
+//     return false;
+// }
+// console.log(arr.filter(greaterthanseven))
+
+//using the reduce methods in array
+
+// const fact= (a,b)=>{
+//     return a*b
+// }
+// console.log(arr.reduce(fact))
+console.log(arr.reduce((val,val2)=>{
+   return val*val2;
+}))
+
+console.log("Numbers greater than 2 :"+arr.filter((val)=>{
+    if(val>2){
         return true;
     }
     return false;
-}
-console.log(arr.filter(greaterthanseven))
+}))
