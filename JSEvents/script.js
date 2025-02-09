8920130abd3c2df61b25665d3bc93cb08ab8fa99
Devarsh Dubey <devarsh.d10@gmail.com>
2025-02-10 01:44:37 +0530

    .

---
+++ JSEvents/script.js
@@ -0,0 +1,7 @@
+let button=document.getElementById("btn");
+button.addEventListener("contextmenu",()=>{
+    document.querySelector(".box").innerHTML="Yehh I have bee clicked"
+})
+// button.addEventListener(,()=>{
+//     document.querySelector(".box").innerHTML="Yehh I have bee clicked"
+document.addEventListener("key")
--- dynamicwebsitebuilding/script.js
+++ dynamicwebsitebuilding/script.js
@@ -1,15 +1,15 @@
-function card(title, chname, views, time, thumbnail,duration) {
-let viewstr;
-if(views<1000000){
-    viewstr=views/1000 +"k";
-}
-else if(views>=1000000){
-    viewstr=views/1000000 +"m"
-}
-else {
-    viewstr=views/1000 +"k"
-}
-  let html=` <div class="container">
+function card(title, chname, views, time, thumbnail, duration) {
+    let viewstr;
+    if (views < 1000000) {
+        viewstr = views / 1000 + "k";
+    }
+    else if (views >= 1000000) {
+        viewstr = views / 1000000 + "m"
+    }
+    else {
+        viewstr = views / 1000 + "k"
+    }
+    let html = ` <div class="container">
         <div class="pic">
             <img src=${thumbnail} alt="">
             <div class="time">${time}</div>
@@ -23,8 +23,8 @@ else {
            </div>
         </div>
     </div>`
-    document.querySelector(".main").innerHTML = document.querySelector(".main").innerHTML +html;
+    document.querySelector(".main").innerHTML = document.querySelector(".main").innerHTML + html;
 }
-card("Introduction to backend || Sigma Web Deveopment Course #2","codewithDev",465000,"32:02","https://i.ytimg.com/vi/WYazkpCQNQw/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCqWabFaQdcnqskWMx5K1dd3BfwjQ",2);
+card("Introduction to backend || Sigma Web Deveopment Course #2", "codewithDev", 465000, "32:02", "https://i.ytimg.com/vi/WYazkpCQNQw/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCqWabFaQdcnqskWMx5K1dd3BfwjQ", 2);
 
 
