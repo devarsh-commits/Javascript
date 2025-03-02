8a941a9d606ca701fa32c9e0db7a424bc0a95dfe
Devarsh Dubey <devarsh.d10@gmail.com>
2025-03-02 23:48:46 +0530

    .

---
+++ javascriptQuestions/script.js
@@ -0,0 +1,44 @@
+// //Question no 1
+// let naam=["ikka","Modiji","Amit Shah","Krrish","BatMan","Luv","Hen","Ben"];
+// let house=[[],[],[],[]];
+// let fun=()=>{
+//     for (const el of naam) {
+//         if(el.length<6){
+//             house[0].push(el);
+//         }
+//         else if(el.length>6 && el.length<8){
+//             house[1].push(el)
+//         }
+//         else if(el.length>8 && el.length<12){
+//             house[2].push(el);
+//         }
+//         else{
+//             house[3].push(el)
+//         }
+//     }
+//     console.log(house)
+// }
+// fun();
+
+//Question no 2;
+// let arr=[1,1,2,5,4,4,6,8,9,9,10,10];
+// let newar=[]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]!=arr[i+1]){
+//         newar.push(arr[i]*2)
+//     }
+//     else{
+//         newar.push(arr[i])
+//     }
+// }
+// console.log(newar)
+
+// let naam="Devarsh";
+// let mirror=naam.split("").reverse().join("");
+// console.log(mirror)
+
+// let paas=prompt("Enter the password where length should be more than 8,First letter Capital,last letter digit and atleast one char is lowercase");
+
+
+
+
---
+++ javascriptQuestions/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+// let paas=prompt("Enter the password where length should be more than 8,First letter Capital,last letter digit and atleast one char is lowercase");
